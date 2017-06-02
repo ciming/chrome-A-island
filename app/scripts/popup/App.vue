135fe17664e7b837778e43a25100dd47de2d521f
buxingxu <5211314>
2017-06-02 16:41:00 +0800

    调整布局

--- app/scripts/popup/App.vue
+++ app/scripts/popup/App.vue
@@ -1,34 +1,38 @@
 <template>
     <div id="app">
-
-        <router-view></router-view>
+        <router-view class="page"></router-view>
         <footer>
-            <a>
+            <router-link :to="{name:'index'}">
               <svg>
                 <use xlink:href="#rrs"></use>
               </svg>
-            </a>
-            <a>
-              <svg>
-                <use xlink:href="#cookie"></use>
-              </svg>
-            </a>
-             <a>
+            </router-link>
+             <router-link :to="{name:'setting'}">
               <svg>
                 <use xlink:href="#setting"></use>
               </svg>
-            </a>
+            </router-link>
         </footer>
     </div>
 
 </template>
+<script>
+  export default{
+    created () {
 
+    }
+  }
+</script>
 <style scoped lang="less">
+  #app{
+    display: flex;
+    flex-direction: column;
+    .page{
+      flex: 1;
+    }
+  }
   footer{
-    position: fixed;
-    bottom: 0;
-    left:0;
-    right: 0;
+   width: 100%;
     height: 45px;
     background-color: #f0f0f0;
     display: flex;
--- app/scripts/popup/router/index.js
+++ app/scripts/popup/router/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Index from '../page/Index.vue'
+import Setting from '../page/Setting.vue'
 
 Vue.use(Router)
 
 const router = new Router({
     routes: [
-        { path: '/', component: Index },
+        { name: 'index', path: '/', component: Index },
+        { name: 'setting', path: '/setting', component: Setting }
     ]
 })
 export default router;
