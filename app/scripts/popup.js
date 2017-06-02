41676ddeaeb52df2d4d164dd01613bc1475f3586
buxingxu <5211314>
2017-06-02 16:13:41 +0800

    配置vuex

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
 import App from './popup/App.vue';
+import store from './popup/store'
 import router from './popup/router'
+import { sync } from 'vuex-router-sync'
 
+sync(store, router)
 
 const app = new Vue({
     router,
+    store,
     render: h => h(App)
 }).$mount('#app')
